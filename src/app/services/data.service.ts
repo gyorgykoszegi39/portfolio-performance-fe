85172293c958909ce87df62d0b4e31424ac3e2a2
gyurika <gyorgykoszegi39@gmail.com>
2024-06-04 15:46:14 +0200

    environments, not environment

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Filter } from '../app.component';
-import { environment } from '../../environment/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
